a3d5c01e14826ed4e4b6c908d6e2e986d744e31b
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-09 15:30:52 -0800

    added routes and navBar links with some styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Projects from './Projects';
+import Connect from './Connect';
+import Resume from './Resume';
+import NavBar from './NavBar';
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className='App'>
+      <Router>
+        <header className='NavLink'>
+          <NavBar />
+        </header>
+        <main>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/projects" element={<Projects />} />
+          <Route exact path="/connect" element={<Connect />} />
+          <Route exact path="/resume" element={<Resume />} />
+        </main>
+      </Router>
     </div>
+    
+    
   );
 }
 
