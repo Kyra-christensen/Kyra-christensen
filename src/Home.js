9dc02853343ce1bac1833c6d29633ca4dff5c69f
Kyra Christensen <christensen.kyral@gmail.com>
2022-03-09 18:05:02 -0800

    added more styling haha

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Sunset from './sunsetCloud.webp';
 
 export default function Home() {
   return (
     <div className='home'>
-      <div className='nameTag'>
+      <div className='nameTag' style={ { backgroundImage: `url(${Sunset})` }}>
         <h1>Kyra L. Christensen</h1>
         <h2>Full-Stack Software Developer</h2>
       </div>
-      
+      <div>
+        <h3>This is just a place where extra text will go.</h3>
+      </div>
     </div>
   );
 }
